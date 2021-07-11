770e2d1b53e7f37ffd214ebe9cfe38c1e780cc1b
ümit <51458506+backspace1990@users.noreply.github.com>
2021-07-12 00:42:49 +0300

    Delete povtor directory

--- povtor/C01/introduction pointers/ft_putadress.c
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-
-void ft_putadress(void *ter)
-{
-    printf("%p\n",ter);
-}
--- povtor/C01/introduction pointers/ft_putnum.c
+++
@@ -1,6 +0,0 @@
-#include <unistd.h>
-
-void ft_putnum(int num)
-{
-    write(1, &num, 1);
-}
